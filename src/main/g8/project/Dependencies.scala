a376e0d7721e75a455ece6eecc7e1bc92896f5c4
Paul <paul@movio.co>
2018-09-19 17:11:53 +1200

    Rename Version.config in Version.typesafeConfig

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,12 +5,12 @@ object Dependencies {
   def testDependencies(list: Seq[ModuleID]): Seq[ModuleID] = list.map(_ % "test,it")
 
   private object Version{
-    lazy val config = "1.3.3"
+    lazy val typesafeConfig = "1.3.3"
     lazy val scalaLogging = "3.9.0"
     lazy val scalatest = "3.0.5"
   }
 
-  val typesafeConfig = "com.typesafe" % "config" % Version.config
+  val typesafeConfig = "com.typesafe" % "config" % Version.typesafeConfig
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % Version.scalaLogging
   val scalatest = "org.scalatest" %% "scalatest" % Version.scalatest
 }
