38340771f6071611ded450f190b0210a431cc56b
Paul Ellis <paul@movio.co>
2018-09-19 16:35:49 +1200

    Update dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,15 +3,12 @@ import sbt._
 object Dependencies {
 
   private object Version{
-    lazy val config = "1.3.1"
-    lazy val scalaLogging = "3.5.0"
-    lazy val scalatest = "3.0.1"
+    lazy val config = "1.3.3"
+    lazy val scalaLogging = "3.9.0"
+    lazy val scalatest = "3.0.5"
   }
 
-  private val config = "com.typesafe" % "config" % Version.config
-  private val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % Version.scalaLogging
-  private val scalatest = "org.scalatest" %% "scalatest" % Version.scalatest
-
-  lazy val utils = Seq[ModuleID](config, scalaLogging)
-  lazy val tests = Seq[ModuleID](scalatest) map (_ % "test,it")
+  val typesafeConfig = "com.typesafe" % "config" % Version.config
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % Version.scalaLogging
+  val scalatest = "org.scalatest" %% "scalatest" % Version.scalatest
 }
